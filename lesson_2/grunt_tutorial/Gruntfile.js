790e60f71360be350c2490126000b5dc7ac1f754
Menachem Korf <menachemkorf@gmail.com>
2017-06-06 13:15:43 +1000

    complete bower and grunt tutorials

---
+++ lesson_2/grunt_tutorial/Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    uglify: {
+      my_target: {
+        files: {
+          'public/javascripts/application.js': ['public/javascripts/all.js']
+        }
+      }
+    },
+    bower_concat: {
+      all: {
+        dest: 'public/javascripts/all.js'
+      }
+    }
+  });
+
+  // Load the plugins that provide the tasks.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-bower-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', ['bower_concat', 'uglify']);
+};
